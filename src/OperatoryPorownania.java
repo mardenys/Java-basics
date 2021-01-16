c6812295999b103c60fbfa79b3c02d006e9186d7
Marcin <mardenys@gmail.com>
2021-01-16 13:18:30 +0100

    Operatory logiczne

---
+++ src/OperatoryPorownania.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// porównują elementy równania i zwracają logiczną wartość (true/false)
+public class OperatoryPorownania {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe a");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Podaj druga liczbe b");
+        int secondNumber = scanner.nextInt();
+
+        boolean result = secondNumber > firstNumber;
+
+        System.out.println("b > a: " + result);
+
+        System.out.println("a > b: " + (firstNumber > secondNumber));
+        System.out.println("a < b: " + (firstNumber < secondNumber));
+        System.out.println("a >= b: " + (firstNumber >= secondNumber));
+        System.out.println("a <= b: " + (firstNumber <= secondNumber));
+        System.out.println("a == b: " + (firstNumber == secondNumber));
+        System.out.println("a != b: " + (firstNumber != secondNumber));
+    }
+}
