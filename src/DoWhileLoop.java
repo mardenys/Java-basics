3cbdbbe3bea6ea4efe5fe5a2d9a56e61158bf927
Marcin <mardenys@gmail.com>
2021-01-17 12:32:46 +0100

    Pętla do while

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int number=15;
+        do {
+            System.out.println("Moja wartosc to: " +number + " i jestem mniejsza od 20");
+            number++;
+        } while (number < 20);
+    }
+}
