189ac5631d9b0e6c2ea5c5db1d245cc699b843ed
Marcin <mardenys@gmail.com>
2021-01-17 12:49:06 +0100

    Tablice

---
+++ src/Tablice.java
@@ -0,0 +1,33 @@
+public class Tablice {
+
+    public static void main(String[] args) {
+        String[] imiona = new String[3];
+        imiona[0] = "Alba";
+        imiona[1] = "Tomasz";
+        imiona[2] = "Marcin";
+
+
+
+        /*System.out.println(imiona[0]);
+        System.out.println(imiona[1]);
+        System.out.println(imiona[2]);*/
+
+        int[] lottoNumbers = new int[] {1,2,3,4,5,6};
+
+        System.out.println(lottoNumbers[0]);
+        System.out.println(lottoNumbers[1]);
+        System.out.println(lottoNumbers[2]);
+        System.out.println(lottoNumbers[3]);
+        System.out.println(lottoNumbers[4]);
+        System.out.println(lottoNumbers[5]);
+
+        System.out.println(lottoNumbers.length);
+
+
+        for (int i=0; i <lottoNumbers.length;i++) {
+            System.out.println(lottoNumbers[i]);
+        }
+
+
+    }
+}
