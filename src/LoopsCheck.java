36b3e230d498b15ed16c6973f112385170d12a61
Marcin <mardenys@gmail.com>
2021-01-17 22:14:44 +0100

    Odwrócone tablice

--- src/LoopsCheck.java
+++ src/LoopsCheck.java
@@ -2,24 +2,18 @@ public class LoopsCheck {
 
     public static void main(String[] args) {
 
-        //liczby z zakresu 1-100 podzielne przez 3
-        /*for (int i =1; i < 100 ;i++) {
-            if(i%3==0)
-                System.out.println(i);
-        }*/
-
 
         //odwrcone elementy tablicy
-        int[] tableElements = new int[] {1,3,5,7,0};
+        int[] numbers = new int[] {1,3,5,7,9};
 
-        for (int i=0; i <(tableElements.length/2);i++) {
-            int temp = tableElements[i]; //dla i=0 1  i=1  3
-            tableElements[i] = tableElements[tableElements.length - 1 - i]; // 4=>0   3=>7
-            tableElements[tableElements.length - 1 - i] = temp; //1  // numbers[3]  =3
+        for (int i=0; i <(numbers.length/2);i++) {
+            int temp = numbers[i]; //dla i=0 1  i=1  3
+            numbers[i] = numbers[numbers.length - 1 - i]; // 4=>9   3=>7
+            numbers[numbers.length - 1 - i] = temp; //1  // numbers[3]  =3
         }
 
-        for (int i=0; i < tableElements.length; i++) {
-            System.out.println(tableElements[i]);
+        for (int i=0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
 
     }
