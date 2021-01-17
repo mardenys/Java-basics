9759526c9748b2deb656a4d3bd63b486298c67bb
Marcin <mardenys@gmail.com>
2021-01-17 23:00:14 +0100

    Tablica odwrócona

--- src/LoopsCheck.java
+++ src/LoopsCheck.java
@@ -4,12 +4,14 @@ public class LoopsCheck {
 
 
         //odwrcone elementy tablicy
-        int[] numbers = new int[] {1,3,5,7,9};
+        int[] numbers = new int[] {1,3,5,7,0};
 
         for (int i=0; i <(numbers.length/2);i++) {
             int temp = numbers[i]; //dla i=0 1  i=1  3
-            numbers[i] = numbers[numbers.length - 1 - i]; // 4=>9   3=>7
+            numbers[i] = numbers[numbers.length - 1 - i]; // 4=>0   3=>7
             numbers[numbers.length - 1 - i] = temp; //1  // numbers[3]  =3
+
+            System.out.println("Iteracja numer " + i);
         }
 
         for (int i=0; i < numbers.length; i++) {
