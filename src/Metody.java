9153db2481c107349d22310dff86b309739b6a4e
Marcin <mardenys@gmail.com>
2021-01-19 13:13:02 +0100

    Metody

--- src/Metody.java
+++ src/Metody.java
@@ -1,15 +1,15 @@
 public class Metody {
 
-    public void policzWynik() {
+   /* public void policzWynik() {
         System.out.println("Zaraz policze wynik");
         int result = 0;
         for (int i =0; i < 100; i++) {
             result = result + i;
         }
         System.out.println("rezultat to: " + result);
-    }
+    }*/
 
-    public int pobierzWynik() {
+    /*public int pobierzWynik() {
         //System.out.println("Zaraz policze wynik");
         int result = 0;
         for (int i =0; i < 100; i++) {
@@ -17,9 +17,9 @@ public class Metody {
         }
         //System.out.println("rezultat to: " + result);
         return result;
-    }
+    }*/
 
-    public void policzWynikParam(int number) {
+    /*public void policzWynikParam(int number) {
         System.out.println("Number ma wartość: " + number);
         System.out.println("Zaraz policze wynik");
         int result = 0;
@@ -27,7 +27,7 @@ public class Metody {
             result = result + i;
         }
         System.out.println("rezultat to: " + result);
-    }
+    }*/
 
     public int add(int firstNumber, int secondNumber, String word) {
         //System.out.println("Suma to: " +(firstNumber+secondNumber));
